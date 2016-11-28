8d51c490acc8498526d0761c6f45f8dc7857fd97
Henrik Paul <henrik@vaadin.com>
2016-11-28 17:15:19 +0200

    initial version of the project progress bar

--- src/components/DashProgress.vue
+++ src/components/DashProgress.vue
@@ -1,9 +1,60 @@
 <template>
+  <div>
+    <div v-for="project in projects">
+      <h1>{{ project.name }}</h1>
+      <ul>
+        <li v-for="highlight in project.highlights">{{ highlight }}</li>
+      </ul>
+      <dash-progress-graph
+        :start="project.graphdata.start"
+        :target="project.graphdata.target"
+        :maybe="project.graphdata.maybe"
+        :probably="project.graphdata.probably"></dash-progress-graph>
+    </div>
+  </div>
 </template>
 
 <script>
+  import DashProgressGraph from './DashProgressGraph'
+
   export default {
-    name: 'dash-progress'
+    name: 'dash-progress',
+    components: {
+      DashProgressGraph
+    },
+    data: function() { return {
+      projects: [
+        {
+          name: 'Spreadsheet 1.3',
+          highlights: [
+            'Newer version of POI',
+            'Support for named ranges',
+            'Support for SUBTOTAL function'
+          ],
+          graphdata: {
+            start: new Date('2016-05-01'),
+            target: new Date('2017-12-01'),
+            maybe: new Date('2017-11-01'),
+            probably: new Date('2018-01-01')
+          }
+        },
+        {
+          name: 'Designer 1.1',
+          highlights: [
+            'IDEA support',
+            'Customizable content in components',
+            'Support variants of Eclipse',
+            'HTML snippets'
+          ],
+          graphdata: {
+            start: new Date('2016-05-01'),
+            target: new Date('2017-12-01'),
+            maybe: new Date('2017-11-01'),
+            probably: new Date('2018-01-01')
+          }
+        }
+      ]
+    } }
   }
 </script>
 
---
+++ src/components/DashProgressGraph.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="graph">
+    <div class="timeline">
+      <div class="progress above" :style="{ width: percentager(todayFraction) }"></div>
+      <div class="maybe above" :style="{ right: percentager(1-maybeFraction) }">maybe</div>
+      <div class="probably above" :style="{ right: percentager(1-probablyFraction) }">probably</div>
+      <div class="start underneath">{{ start | dateformatter }}</div>
+      <div class="target underneath" :style="{ right: percentager(1-targetFraction) }">{{ target | dateformatter }} ^</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dash-progress-graph',
+    props: {
+      start: { type: Date, required: true },
+      target: { type: Date, required: true },
+      maybe: { type: Date, required: true },
+      probably: { type: Date, required: true }
+    },
+    computed: {
+      startTime: function() {
+        return this.start.getTime();
+      },
+      maxTime: function () {
+        return (this.target > this.probably) ? this.target.getTime() : this.probably.getTime();
+      },
+      maxTimeRelative: function () {
+        return this.maxTime - this.startTime;
+      },
+      todayFraction: function () {
+        return (new Date().getTime() - this.startTime) / this.maxTimeRelative;
+      },
+      maybeFraction: function () {
+        return (this.maybe.getTime() - this.startTime) / this.maxTimeRelative;
+      },
+      probablyFraction: function () {
+        return (this.probably.getTime() - this.startTime) / this.maxTimeRelative;
+      },
+      targetFraction: function () {
+        return (this.target.getTime() - this.startTime) / this.maxTimeRelative;
+      }
+    },
+    filters: {
+      /** @param {Date} date */
+      dateformatter: function(date) {
+        return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
+      }
+    },
+    methods: {
+      /** @param {number} number */
+      percentager: function(number) {
+        return (number * 100) + '%';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .graph {
+    width: 100%;
+    position: relative;
+    box-sizing: border-box;
+  }
+
+  .timeline {
+    border-left: 2px solid black;
+    border-bottom: 2px solid black;
+    box-sizing: border-box;
+    height: 20px;
+  }
+
+  .progress {
+    background-color: #ddd;
+    height: 100%;
+  }
+
+  .start {
+    left: -20px;
+  }
+
+  .underneath, .above {
+    position: absolute;
+  }
+
+  .underneath {
+    top: 20px;
+  }
+
+  .above {
+    top: 0;
+  }
+
+  .start {
+    position: absolute;
+  }
+
+  .maybe, .probably {
+    border-right: 2px solid black;
+  }
+</style>
